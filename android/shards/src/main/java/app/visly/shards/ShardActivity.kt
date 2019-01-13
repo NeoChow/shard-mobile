ead26141f3d88ba4ce655d8c14d5f64b8a476184
Emil Sjölander <emil@visly.app>
2019-01-13 10:28:46 -0800

    add open url support to android

--- android/shards/src/main/java/app/visly/shards/ShardActivity.kt
+++ android/shards/src/main/java/app/visly/shards/ShardActivity.kt
@@ -13,6 +13,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import app.visly.vml.VMLRootView
 import app.visly.vml.VMLViewManager
+import android.content.Intent
+import android.net.Uri
+import app.visly.vml.JsonValue
+
 
 class ShardActivity : AppCompatActivity() {
     lateinit var root: VMLRootView
@@ -27,6 +31,10 @@ class ShardActivity : AppCompatActivity() {
 //        val revision = intent.extras!!.getInt("revision")!!
 
         root = findViewById(R.id.vml_root)
+        root.on("open-url") {
+            val url = (it as JsonValue.String).value
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+        }
 
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         toolbar.inflateMenu(R.menu.activity_shard)
