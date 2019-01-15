c37747e55fbf8d204ae20ea157870fd56009c28f
Anna Viklund <anna.viklund@hotmail.com>
2019-01-15 11:35:46 -0800

    Slide in example view

--- ios/Example/ShardKit/ExamplesLauncher.swift
+++ ios/Example/ShardKit/ExamplesLauncher.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ExamplesLauncher: NSObject {
     let backgroundView = UIView()
+    let exampleView = UIView()
     
     override init() {
         super.init()
@@ -17,22 +18,56 @@ class ExamplesLauncher: NSObject {
     
     func showExample() {
         if let window = UIApplication.shared.keyWindow {
-            backgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-            backgroundView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissExample)))
+            setupBackgroundView(inWindow: window)
             
-            window.addSubview(backgroundView)
-            backgroundView.frame = window.frame
-            backgroundView.alpha = 0
+            exampleView.backgroundColor = UIColor.white
             
-            UIView.animate(withDuration: 0.5, animations: {
+            let exampleWidth = window.frame.width
+            let exampleHeight: CGFloat = 128
+            let initY = window.frame.height
+            let finalY = window.frame.height - exampleHeight
+            
+            window.addSubview(exampleView)
+            exampleView.frame = CGRect(
+                x: 0,
+                y: initY,
+                width: exampleWidth,
+                height: exampleHeight
+            )
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                 self.backgroundView.alpha = 1
-            })
+                self.exampleView.frame = CGRect(
+                    x: 0,
+                    y: finalY,
+                    width: exampleWidth,
+                    height: exampleHeight
+                )
+            }, completion: nil)
         }
+    }
+    
+    func setupBackgroundView(inWindow window: UIWindow) {
+        backgroundView.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        backgroundView.alpha = 0
+        backgroundView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissExample)))
+        
+        window.addSubview(backgroundView)
+        backgroundView.frame = window.frame
     }
     
     @objc func dismissExample() {
         UIView.animate(withDuration: 0.5, animations: {
             self.backgroundView.alpha = 0
+            
+            if let window = UIApplication.shared.keyWindow {
+                self.exampleView.frame = CGRect(
+                    x: 0,
+                    y: window.frame.height,
+                    width: self.exampleView.frame.width,
+                    height: self.exampleView.frame.height
+                )
+            }
         })
     }
 }
